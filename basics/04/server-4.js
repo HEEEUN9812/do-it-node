d7c3b6ec869666264606fa6e88617c19f1319f95
HEEEUN9812 <heeeun9812@gmail.com>
2024-12-11 00:56:49 +0900

    2024-12-11 섹션 2 완

--- basics/04/server-4.js
+++ basics/04/server-4.js
@@ -1,2 +1,11 @@
 // HTML 페이지 서빙하기  (결과 비교 파일 : 04\results\server-4.js)
 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log("요청 발생");
+});
+
+server.listen(3000, () => {
+    console.log("서버가 실행 중");
+})
