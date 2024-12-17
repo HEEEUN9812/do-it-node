c977a708cba04c2a2df2d996f494eb5a3e8ae4b3
heeeun <heeeun12@bizpeer.co.kr>
2024-12-17 10:15:01 +0900

    2024-12-17 섹션 3 완

--- myContacts/controllers/contactController.js
+++ myContacts/controllers/contactController.js
@@ -1,28 +1,53 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
-const getAllContacts = asyncHandler(async(req,res) => {
-    res.send("Contacts Page");
+const getAllContacts = asyncHandler(async (req, res) => {
+    const contacts = await Contact.find();
+    res.send(contacts);
 });
 
-const createContact = asyncHandler(async (req,res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log(req.body);
-    const {name, age} = req.body;
-    if(!name || !age) {
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
         return res.send("필수값이 입력되지 않았습니다");
     }
+    const contact = await Contact.create({
+        name, phone, email
+    });
     res.send("create contacts");
 })
 
-const getContact = asyncHandler(async (req,res) => {
-    res.send(`view contact for ID : ${req.params.id}`)
+const getContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    res.send(contact);
 })
 
-const updateContact = asyncHandler(async (req,res) =>{
-    res.send(`update contact for ID : ${req.params.id}`);
+const updateContact = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const {name, email, phone} = req.body;
+    const contact = await Contact.findById(id);
+    if(!contact) {
+        throw new Error("Contact not found.");
+    }
+
+    contact.name = name;
+    contact.email = email;
+    contact.phone = phone;
+
+    contact.save();
+
+    res.json(contact);
 })
 
-const deleteContact = asyncHandler(async (req,res) =>{
-    res.send(`delete contact for ID : ${req.params.id}`);
+const deleteContact = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const contact = await Contact.findById(id);
+    if(!contact) {
+        throw new Error("Contact not found.");
+    }
+    await contact.deleteOne();
+    res.send("Deleted");
 })
 
-module.exports = {getAllContacts, createContact, getContact, updateContact, deleteContact};
+module.exports = { getAllContacts, createContact, getContact, updateContact, deleteContact };
